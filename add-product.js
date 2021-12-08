e9425c2ebb88c9468249dffec168dc8e8551f7b1
TruongCV <viettruong2495@gmail.com>
2021-12-08 16:09:58 +0700

    1

--- add-product.js
+++ add-product.js
@@ -1,26 +1,26 @@
 // phần này chúng ta thao tác với localStorage
 // kiểm tra xem products đã tồn tại trong localStorage hay chưa 
+// true || false 
+// !true = false
+// !false = true
 if (!localStorage.products) {
      // nếu chưa thì tạo mới bằng 1 mảng rỗng
      let arr = [];
      // do localStorage chỉ cho phép lưu chữ dữ liệu dạng chuỗi json nên ta sử dung (ae cũng có thể search xem json là gì)
      localStorage.products = JSON.stringify(arr);
 }
-
+// let hello = 'hello';
 // Hàm đọc dữ liệu từ localStorage đổ vào bảng => đặt ở đây để ngay khi load trang xong sẽ chạy hàm này để show sản phẩm
-showProductInTable();
-
+if (JSON.parse(localStorage.products).length > 0) {
+     showProductInTable();
+}
 function addProduct() {
+     // B1
      // lấy value từ các input trong form theo id
      let product_name = document.getElementById('product_name').value;
      let product_image = document.getElementById('product_image').value;
      let product_description = document.getElementById('product_description').value;
      let product_type = document.getElementById('product_type').value;
-
-     console.log('product_name: ' + product_name)
-     console.log('product_image: ' + product_image)
-     console.log('product_description:' + product_description)
-     console.log('product_type: ' + product_type);
 
      // dựa vào các dữ liệu lấy được từ form ta tạo 1 object product
      let product = {
@@ -31,14 +31,17 @@ function addProduct() {
           description: product_description,
           type: product_type,
      }
-
+     // het b1
      // tạo 1 biến products để hứng dữ liệu lấy đc từ localStorage.products 
      // đọc dữ liệu từ localStorage.products ra do dữ liệu từ localStorage.products trả về là 1 chuỗi nên ta cần convert chuỗi json này về mảng để sử dụng được hàm array.push() để add thêm phần từ vào mảng
-     let products = JSON.parse(localStorage.products);
+//     b2
+     let products = JSON.parse(localStorage.products); // mảng products
 
+     // b3
      // sử dụng được array.push() để add thêm phần từ vào mảng products
      products.push(product);
 
+     // b4
      // việc còn lại là gán localStorage.products bằng mảng products mới vừa đc add thêm phần tử => nhớ convert array products sang dạng json nha ae
      localStorage.products = JSON.stringify(products);
 
@@ -65,6 +68,7 @@ function showProductInTable() {
           // stringHtml += 'abc'   
           // là 1 cách viết tắt của 
           // stringHtml = stringHtml + 'abc'
+        
           stringHtml += `
                <tr>
                     <td><img src="${product.image}" width="50px" height="50px"></td>
@@ -80,6 +84,14 @@ function showProductInTable() {
      tbody.innerHTML = stringHtml;
 
      console.log(tbody)
+
+     let name = 'aa';
+     let tuoi = 'aa';
+     let sdt = 'aa';
+     let gioithieu = 'đây là ' + name + ' nam nay ' + tuoi + ' tuoi sdt la ' + sdt;
+     let gioithieu2 = `đât là ${name} name nay ${tuoi} tuoi sdt là ${sdt}`;
+     // tạo 1 add student.html có 1 input và 1 nút để click
+     // 
 }
 
 
--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log(111);
+
+if (!localStorage.products) {
+     // nếu chưa thì tạo mới bằng 1 mảng rỗng
+     let arr = [];
+     // do localStorage chỉ cho phép lưu chữ dữ liệu dạng chuỗi json nên ta sử dung (ae cũng có thể search xem json là gì)
+     localStorage.products = JSON.stringify(arr);
+}
+// let hello = 'hello';
+// Hàm đọc dữ liệu từ localStorage đổ vào bảng => đặt ở đây để ngay khi load trang xong sẽ chạy hàm này để show sản phẩm
+if (JSON.parse(localStorage.products).length > 0) {
+     showProductDiv();
+}
+let stringHtml = ``;
+
+
+
+
+
+function showProductDiv(){
+     console.log('showProductDiv');
+     let divProduct = document.getElementById("product-list");
+     let products = JSON.parse(localStorage.products);
+     let stringHtml = '';
+     for (let i = 0; i < products.length; i++) {
+          let product = products[i];
+          stringHtml += `
+          <div class="col-sm-3">
+               <img src="${product.image}" alt="">
+               <p>${product.name}</p>
+               <p>${product.description}</p>
+          </div>
+          `;
+     }
+     divProduct.innerHTML = stringHtml;
+}
